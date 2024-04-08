f688bf20907ec7528df3a4a036a5d690e6d24d0e
dlrudgjs104 <92204410+dlrudgjs104@users.noreply.github.com>
2024-04-08 10:27:36 +0900

    [Bronze V] Title: A+B - 8, Time: 260 ms, Memory: 18604 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int t = scanner.nextInt();
+        StringBuilder str = new StringBuilder();
+
+        for (int i = 1; i <= t; i++) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            
+            str.append(String.format("Case #%d: %d + %d = %d\n", i, a, b, a + b));
+        }
+        System.out.println(str);
+    }
+}
