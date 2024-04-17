9a4126987add126195f7129fd775f5ccbae56ba5
dlrudgjs104 <92204410+dlrudgjs104@users.noreply.github.com>
2024-04-17 20:56:40 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 992 ms, Memory: 42300 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+
+        int[][] a = new int[n][m];
+        int[][] b = new int[n][m];
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                a[i][j] = scanner.nextInt();
+            }
+        }
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                b[i][j] = scanner.nextInt();
+            }
+        }
+        
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                a[i][j] += b[i][j];
+            }
+        }
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                System.out.printf("%d ", a[i][j]);
+            }
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+}
