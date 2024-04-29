fc494e709bafdd5d5e8f58693c2bc6c30fe5ab3b
dlrudgjs104 <92204410+dlrudgjs104@users.noreply.github.com>
2024-04-30 08:51:09 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 212 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        long n = scanner.nextLong();
+
+        if (n <= 2) {
+            System.out.println((n * n * n - 3 * n * n + 2 * n) / 6);
+        } else {
+            System.out.println((n * n * n - 3 * n * n + 2 * n) / 6);
+        }
+
+        System.out.println(3);
+        
+        scanner.close();
+    }
+}
