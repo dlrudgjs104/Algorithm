0269ab82079fa5c2a7972a10015180de1b664cd5
이경헌 <92204410+dlrudgjs104@users.noreply.github.com>
2025-04-02 02:07:15 +0900

    [Bronze V] Title: 상근이의 친구들, Time: 184 ms, Memory: 17420 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        while(true) {
+            int a = s.nextInt();
+            int b = s.nextInt();
+
+            if (a == 0) return;
+
+            System.out.println(a + b);
+        }
+    
+    }
+
+}
+
