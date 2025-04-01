bc5ffd3419339443ad47bf691eabdb4625e310af
이경헌 <92204410+dlrudgjs104@users.noreply.github.com>
2025-04-02 01:51:36 +0900

    [Bronze V] Title: N 찍기, Time: 772 ms, Memory: 32416 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int input = s.nextInt();
+        for (int i = 1; i <= input; i++) {
+            System.out.println(i);
+        }
+    }
+
+}
+
